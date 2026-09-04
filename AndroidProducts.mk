93c297ba70b80c15c2a0d65186eb396a11a659b2
Farriz Design Pro <garenafreefiremax1339@gmail.com>
2026-09-04 13:15:52 +0700

    feat: add custom_serenity.mk for custom ROM seventeen
    - add custom_serenity.mk inheriting from vendor/custom
    - update AndroidProducts.mk with custom lunch choices cp2a

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,6 +5,7 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_serenity.mk \
+    $(LOCAL_DIR)/custom_serenity.mk \
     $(LOCAL_DIR)/lineage_serenity.mk \
     $(LOCAL_DIR)/pixelos_serenity.mk
 
@@ -12,6 +13,9 @@ COMMON_LUNCH_CHOICES := \
     aosp_serenity-cp2a-user \
     aosp_serenity-cp2a-userdebug \
     aosp_serenity-cp2a-eng \
+    custom_serenity-cp2a-user \
+    custom_serenity-cp2a-userdebug \
+    custom_serenity-cp2a-eng \
     lineage_serenity-cp2a-user \
     lineage_serenity-cp2a-userdebug \
     lineage_serenity-cp2a-eng \
